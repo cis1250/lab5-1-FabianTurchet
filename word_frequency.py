52c74c398946002d2cbd14f89b743b3160639122
FabianTurchet <fturchet@uoguelph.ca>
2025-10-21 11:44:05 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -4,6 +4,7 @@
 # TODO: (Read detailed instructions in the Readme file)
 
 import re
+import string
 
 #This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
 def is_sentence(text):
@@ -24,3 +25,39 @@ def is_sentence(text):
         return False
 
     return True
+
+def get_sentence():
+    sentence = input("Enter a sentence: ")
+    while not is_sentence(sentence):
+        print("This does not meet the criteria for a sentence.")
+        sentence = input("Enter a sentence: ")
+    return sentence
+
+def calculate_frequencies(sentence):
+    translator = str.maketrans("", "", string.punctuation)
+    words = sentence.lower().translate(translator).split()
+
+    word_list = []
+    freq_list = []
+
+    for w in words:
+        if w in word_list:
+            idx = word_list.index(w)
+            freq_list[idx] += 1
+        else:
+            word_list.append(w)
+            freq_list.append(1)
+    return word_list, freq_list
+
+def print_frequencies(words, freqs):
+    print("\nWord Frequencies:")
+    for i in range(len(words)):
+        print(f"{words[i]}: {freqs[i]}")
+
+def main():
+    sentence = get_sentence()
+    words, freqs = calculate_frequencies(sentence)
+    print_frequencies(words, freqs)
+
+main()
+    
